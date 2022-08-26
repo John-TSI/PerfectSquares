92aa9ae09377014605e6580720cf1010985ddd6a
John Roughley <jroughley@softwareinstitute.com>
2022-08-26 09:45:11 +0100

    minor changes, added comments

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main()
 	std::cout << Solution().numSquares(12) << '\n';  // 4+4+4 --> 3
 	std::cout << Solution().numSquares(104) << '\n';  // 100+4 --> 2
 	std::cout << Solution().numSquares(14) << '\n';  // 9+4+1 --> 3
-	std::cout << Solution().numSquares(5) << '\n';  // 1+4 --> 2
+	std::cout << Solution().numSquares(5) << '\n';  // 4+1 --> 2
 	return 0;
 }
 
--- src/solution.cpp
+++ src/solution.cpp
@@ -2,35 +2,25 @@
 #include"../inc/solution.hpp"
 
 
-/* int Solution::numSquares(int n)
-{
-    if(isPerfectSquare(n)){ return 1; }
-
-    int count{0}, rem{n}, sub{1};
-    while(true)
-    {
-        if(isPerfectSquare(rem)){ return ++count; }
-        rem -= pow(sub++,2);
-        if(rem == 1){ return ++count; }
-        if(rem < 0){ n -= pow(sub-2,2); rem = n; sub = 1; ++count; continue; }
-        if(isPerfectSquare(rem)){ rem = n-rem;  ++count; continue; }
-        rem = n;
-    }
-} */
-
+// idea is to subtract from n increasingly large perfect squares, until finding another perf square
 int Solution::numSquares(int n)
 {
-    int count{0}, tmp{n}, sub{1};
+    int count{0}, tmp{n}, sub{1};  // counts perf squares / stores original input / value to be squared and subtracted, respectively
     while(true)
     {
-        if(isPerfectSquare(n)){ return ++count; }
-        n -= pow(sub++,2);
-        if(n == 1){ return ++count; }
+        if(isPerfectSquare(n)){ return ++count; }  // found a perf square
+        n -= pow(sub++,2);  // subtract next largest perfect square from n
+
+        // have subtracted too large of a perfect square from n. subtract the previous largest perf square
+        // from input tmp, reset n to tmp, and then repeat process for this smaller value (incrementing count)
         if(n < 0){ tmp -= pow(sub-2,2); n = tmp; sub = 1; ++count; continue; }
-        if(isPerfectSquare(n)){ n = tmp-n; ++count; continue; }
-        n = tmp;
+
+        if(n == 1){ return ++count; }  // 1 is the smallest perfect square we can use
+        if(isPerfectSquare(n)){ return count+2; }  // subtraction result is a perf sq (+1), as is the subtracted value (+1)
+
+        n = tmp;  // n is a non-perf square > 1, reset n and try subtracting a larger perfect square
     }
 }
 
-
+// helper function, uses fact that sqrt of a non-perfect square will be non-integer result cast as int
 bool Solution::isPerfectSquare(int n){ return static_cast<int>(sqrt(n))*sqrt(n) == n; }   
